35b152dcba8759a6036bb9cf8b8f465bda853afb
majd7772233-a11y <majd7772233@gmail.com>
2026-08-21 12:28:54 +0300

    Refactor signingConfigs for better readability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,19 +25,23 @@ android {
 
   signingConfigs {
     create("release") {
-      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
-      storeFile = file(keystorePath)
-      storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
-      keyPassword = System.getenv("KEY_PASSWORD")
+        val keystorePath =
+            System.getenv("KEYSTORE_PATH")
+                ?: "${rootDir}/my-upload-key.jks"
+
+        storeFile = file(keystorePath)
+        storePassword = System.getenv("STORE_PASSWORD")
+        keyAlias = System.getenv("KEY_ALIAS")
+        keyPassword = System.getenv("KEY_PASSWORD")
     }
+
     create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
+        storeFile = file("${rootDir}/debug.keystore")
+        storePassword = "android"
+        keyAlias = "androiddebugkey"
+        keyPassword = "android"
     }
-  }
+}
 
   buildTypes {
     release {
